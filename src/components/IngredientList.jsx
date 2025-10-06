15393e349cd1fa62f4943abb02cec7c7c8ee6e96
macooo3 <marcoysu765@gmail.com>
2025-10-05 23:55:43 -0400

    created ingredientList component

---
+++ src/components/IngredientList.jsx
@@ -0,0 +1,19 @@
+export default function IngredientList() {
+     return  {ingredients.length > 0 && (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientList}
+          </ul>
+          {ingredients.length > 3 && (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+              </div>
+              <button onClick={toggleShowRecipe}>Get a recipe</button>
+            </div>
+          )}
+        </section>
+      )}
+}
